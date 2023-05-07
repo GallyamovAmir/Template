f5e8c8ad661ec21b0ebeefef7a31e322fbe4d7a0
GallyamovAmir <amirgallyamov9967@gmail.com>
2023-05-07 18:11:18 +0300

    Реализовал метод для экспорта данных из БД в Excel файл

--- Template_4333/4333_Gallyamov.xaml.cs
+++ Template_4333/4333_Gallyamov.xaml.cs
@@ -73,6 +73,90 @@ namespace Template_4333
             }
         }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            List<tableispro2> category_1;
+            List<tableispro2> category_2;
+            List<tableispro2> category_3;
+            List<tableispro2> category_4;
+            List<tableispro2> category_5;
+            List<tableispro2> category_6;
+            List<tableispro2> category_7;
+            List<tableispro2> category_8;
+            List<tableispro2> category_9;
+
+
+            using (isrpo2Entities2 isrpoEntities = new isrpo2Entities2())
+            {
+
+                category_1 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "2 часа").ToList();
+                category_2 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "4 часа").ToList();
+                category_3 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "6 часов").ToList();
+                category_4 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "320 минут").ToList();
+                category_5 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "480 минут").ToList();
+                category_6 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "10 часов").ToList();
+                category_7 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "12 часов").ToList();
+                category_8 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "120 минут").ToList();
+                category_9 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "600 минут").ToList();
+            }
+
+            var allCategories = new List<List<tableispro2>>()
+            {
+                category_1,
+                category_2,
+                category_3,
+                category_4,
+                category_5,
+                category_6,
+                category_7,
+                category_8,
+                category_9
+            };
+
+            var app = new Excel.Application();
+            app.SheetsInNewWorkbook = 9;
+            Excel.Workbook workbook = app.Workbooks.Add(Type.Missing);
+
+            for (int i = 0; i < 9; i++)
+            {
+                int startRowIndex = 1;
+                Excel.Worksheet worksheet = app.Worksheets.Item[i + 1];
+                worksheet.Name = $"Категория {i + 1}";
+                worksheet.Cells[1][startRowIndex] = "Айди";
+                worksheet.Cells[1][startRowIndex].Font.Bold = true;
+                worksheet.Cells[2][startRowIndex] = "КодЗаказ";
+                worksheet.Cells[2][startRowIndex].Font.Bold = true;
+                worksheet.Cells[3][startRowIndex] = "Датасоздания";
+                worksheet.Cells[3][startRowIndex].Font.Bold = true;
+                worksheet.Cells[4][startRowIndex] = "АйдиКлиент";
+                worksheet.Cells[4][startRowIndex].Font.Bold = true;
+                worksheet.Cells[5][startRowIndex] = "Услуга";
+                worksheet.Cells[5][startRowIndex].Font.Bold = true;
+
+                foreach (var person in allCategories[i])
+                {
+                    startRowIndex++;
+                    worksheet.Cells[1][startRowIndex] = person.Айди;
+                    worksheet.Cells[2][startRowIndex] = person.КодЗаказ;
+                    worksheet.Cells[3][startRowIndex] = person.Датасоздания;
+                    worksheet.Cells[4][startRowIndex] = person.АйдиКлиент;
+                    worksheet.Cells[5][startRowIndex] = person.Услуга;
+                }
+                Excel.Range rangeBorders = worksheet.Range[worksheet.Cells[1][1], worksheet.Cells[5][startRowIndex]];
+                rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeBottom].LineStyle =
+                    rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeLeft].LineStyle =
+                    rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeTop].LineStyle =
+                    rangeBorders.Borders[Excel.XlBordersIndex.xlEdgeRight].LineStyle =
+                    rangeBorders.Borders[Excel.XlBordersIndex.xlInsideHorizontal].LineStyle =
+                    rangeBorders.Borders[Excel.XlBordersIndex.xlInsideVertical].LineStyle =
+                    Excel.XlLineStyle.xlContinuous;
+
+                worksheet.Columns.AutoFit();
+            }
+
+            app.Visible = true;
+        }
+
 
     }
 }
