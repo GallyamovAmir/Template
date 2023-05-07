88b79c95230e8f6219733f869c7e154adc1c0dac
GallyamovAmir <amirgallyamov9967@gmail.com>
2023-05-07 18:20:28 +0300

    Реализовал метод экспорта данных из БД  в Word, закончил выполнение 3 лабораторной работы по ИСРПО

--- Template_4333/4333_Gallyamov.xaml.cs
+++ Template_4333/4333_Gallyamov.xaml.cs
@@ -234,6 +234,118 @@ namespace Template_4333
                 MessageBox.Show("Успешный импорт");
             }
         }
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+
+            List<tableispro2> clients = new List<tableispro2>();
+            using (isrpo2Entities2 db = new isrpo2Entities2())
+            {
+                clients = db.tableispro2.ToList();
+                var app = new Word.Application();
+                Word.Document document = app.Documents.Add();
+
+                List<tableispro2> category_1;
+                List<tableispro2> category_2;
+                List<tableispro2> category_3;
+                List<tableispro2> category_4;
+                List<tableispro2> category_5;
+                List<tableispro2> category_6;
+                List<tableispro2> category_7;
+                List<tableispro2> category_8;
+                List<tableispro2> category_9;
+
+                using (isrpo2Entities2 isrpoEntities = new isrpo2Entities2())
+                {
+                    category_1 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "2 часа").ToList();
+                    category_2 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "120 минут").ToList();
+                    category_3 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "4 часа").ToList();
+                    category_4 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "6 часов").ToList();
+                    category_5 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "320 минут").ToList();
+                    category_6 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "480 минут").ToList();
+                    category_7 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "10 часов").ToList();
+                    category_8 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "600 минут").ToList();
+                    category_9 = isrpoEntities.tableispro2.Where(x => x.Времяпроката == "12 часов").ToList();
+                }
+
+                var allCategories = new List<List<tableispro2>>()
+                {
+                    category_1,
+                    category_2,
+                    category_3,
+                    category_4,
+                    category_5,
+                    category_6,
+                    category_7,
+                    category_8,
+                    category_9
+                };
+
+                int i = 1;
+                foreach (var category in allCategories)
+                {
+                    Word.Paragraph paragraph = document.Paragraphs.Add();
+                    Word.Range range = paragraph.Range;
+                    range.Text = "Категория " + i;
+                    i++;
+                    paragraph.set_Style("Заголовок 1");
+                    range.InsertParagraphAfter();
+
+                    Word.Paragraph tableParagraph = document.Paragraphs.Add();
+                    Word.Range tableRange = tableParagraph.Range;
+                    Word.Table clientTable = document.Tables.Add(tableRange, category.Count() + 1, 5);
+                    clientTable.Borders.InsideLineStyle = clientTable.Borders.OutsideLineStyle = Word.WdLineStyle.wdLineStyleSingle;
+                    clientTable.Range.Cells.VerticalAlignment = Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+
+                    Word.Range cellRange;
+                    cellRange = clientTable.Cell(1, 1).Range;
+                    cellRange.Text = "Айди";
+                    cellRange = clientTable.Cell(1, 2).Range;
+                    cellRange.Text = "КодЗаказ";
+                    cellRange = clientTable.Cell(1, 3).Range;
+                    cellRange.Text = "Датасоздания";
+                    cellRange = clientTable.Cell(1, 4).Range;
+                    cellRange.Text = "АйдиКлиент";
+                    cellRange = clientTable.Cell(1, 5).Range;
+                    cellRange.Text = "Услуга";
+                    clientTable.Rows[1].Range.Bold = 1;
+                    clientTable.Rows[1].Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+
+                    int j = 1;
+                    foreach (var person in category)
+                    {
+                        cellRange = clientTable.Cell(j + 1, 1).Range;
+                        cellRange.Text = person.Айди.ToString();
+                        cellRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                        cellRange = clientTable.Cell(j + 1, 2).Range;
+                        cellRange.Text = person.КодЗаказ.ToString(); ;
+                        cellRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                        cellRange = clientTable.Cell(j + 1, 3).Range;
+                        cellRange.Text = person.Датасоздания.ToString();
+                        cellRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                        cellRange = clientTable.Cell(j + 1, 4).Range;
+                        cellRange.Text = person.АйдиКлиент.ToString();
+                        cellRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                        cellRange = clientTable.Cell(j + 1, 5).Range;
+                        cellRange.Text = person.Услуга.ToString();
+                        cellRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                        j++;
+                    }
+
+                    Word.Paragraph DateParagraph = document.Paragraphs.Add();
+                    Word.Range FirstDate = DateParagraph.Range;
+                    Word.Range LastDate = DateParagraph.Range;
+                    LastDate.Text = $"Дата последнего заказа - {category.Last().Датасоздания}";
+                    LastDate.InsertParagraphAfter();
+                    FirstDate.Text = $"Дата первого заказа - {category.First().Датасоздания}";
+                    FirstDate.InsertParagraphAfter();
+
+
+                    document.Words.Last.InsertBreak(Word.WdBreakType.wdPageBreak);
+                }
+
+                app.Visible = true;
+            }
+        }
 
     }
 }
